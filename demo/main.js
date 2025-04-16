9888a4b0505f8f449d7c9f6c12cf79fc33572afe
Agustin <agustin_.gonzalez_@hotmail.com>
2025-04-16 14:10:51 -0300

    fix: fix

--- demo/main.js
+++ demo/main.js
@@ -1,13 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Importa el tema específico desde @primeuix/themes
-import Aura from '@primeuix/themes/aura'
+
 import Lara from '@primeuix/themes/lara'
 
-// Importa iconos y primeflex
-import 'primeicons/primeicons.css'
-import 'primeflex/primeflex.css'
+import 'primeflex/primeflex.css';
 
 import PrimeVue from 'primevue/config';
 const app = createApp(App);
@@ -17,11 +14,7 @@ app.use(PrimeVue,
         theme: {
             preset: Lara,
             options: {
-                prefix: 'p',
-                darkModeSelector: 'lara-dark',
-                lightModeSelector: 'lara-light',
-                themeSelector: 'lara',
-                cssLayer: false
+
             }
         }
     }
