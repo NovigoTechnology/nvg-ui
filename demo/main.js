30e66f6a10642fa49c251841aa2df26fb01be161
Agustin <agustin_.gonzalez_@hotmail.com>
2025-04-17 09:22:25 -0300

    fix: definitive

--- demo/main.js
+++ demo/main.js
@@ -1,17 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Importa el tema específico desde @primeuix/themes
 import Aura from '@primeuix/themes/aura'
-
-// Importa iconos y primeflex
-// import 'primeicons/primeicons.css'
-// import 'primeflex/primeflex.css'
 
 import PrimeVue from 'primevue/config';
 const app = createApp(App);
 
 app.use(PrimeVue, {
-    theme: { preset: Aura }                       // ← aquí se inyecta el CSS
+    theme: { preset: Aura }
   })
 app.mount('#app');
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-// src/index.js  (entry de la librería)
 import PrimeVue from 'primevue/config'
-import Lara from '@primeuix/themes/lara'   // preset por defecto
+import Lara from '@primeuix/themes/lara'
 import MiComponente from './components/MiComponente.vue'
 
 const components = { MiComponente }
@@ -8,11 +7,10 @@ const components = { MiComponente }
 export default {
   /** Instala NvgUI como plugin */
   install(app, { preset = Lara, primevue = {} } = {}) {
-    // Evita instalar PrimeVue dos veces si el usuario ya lo hizo
     if (!app._primevueInstalled) {
       app.use(PrimeVue, {
-        theme: { preset },                 // genera el CSS del tema  :contentReference[oaicite:0]{index=0}
-        ...primevue                        // le dejás pasar opciones personales
+        theme: { preset },
+        ...primevue
       })
       app._primevueInstalled = true
     }
@@ -24,5 +22,4 @@ export default {
   }
 }
 
-// exportación named por si alguien quiere importar uno a uno
 export { MiComponente }
