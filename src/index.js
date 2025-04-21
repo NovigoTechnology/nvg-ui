a39d73551fdc39758f083de5a84b23d11497b353
Agustin <agustin_.gonzalez_@hotmail.com>
2025-04-21 12:58:42 -0300

    fix: new styles

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import PrimeVue from 'primevue/config'
 import Lara from '@primeuix/themes/lara'
 import AutoComplete from './components/AutoComplete.vue'
+import baseStyles from './styles/base.css?raw'
 
 const components = { AutoComplete }
 
 export default {
-  /** Instala NvgUI como plugin */
-  install(app, { themes = { light: Lara }, primevue = {} } = {}) {
+  install(app, { preset = Lara, primevue = {} } = {}) {
     if (!app._primevueInstalled) {
       app.use(PrimeVue, {
-        theme: themes,
+        theme: { preset },
         ...primevue
       })
       app._primevueInstalled = true
     }
+
+    // Inyectamos los estilos desde el archivo CSS
+    const styleSheet = document.createElement('style')
+    styleSheet.textContent = baseStyles
+    document.head.appendChild(styleSheet)
 
     Object.entries(components).forEach(([name, cmp]) =>
       app.component(name, cmp)
