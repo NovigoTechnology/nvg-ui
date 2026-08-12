5dacfb795feee22f8408ad19de18ed2b1e816501
Agustin <agustin_.gonzalez_@hotmail.com>
2026-08-12 13:55:20 -0300

    fix: rename class in autocomplete icon

--- src/components/AutoComplete.vue
+++ src/components/AutoComplete.vue
@@ -60,7 +60,7 @@
         </FloatLabel>
         <InputIcon
           v-if="!disabled && selectedName"
-          class="pi pi-arrow-right autocomplete-scan-icon"
+          class="pi pi-arrow-right autocomplete-icon-field"
           @click="goToDoc"
         />
       </IconField>
